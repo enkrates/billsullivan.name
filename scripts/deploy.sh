abcf82572ae408b0856aaa9cf7077f320b3980b5
William Sullivan <william@enkrates.com>
2019-08-26 21:48:12 -0500

    Adding a wildcard clodufront invalidation to the deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,3 +3,5 @@
 # Make sure to set aws_config correctly to make this work.
 
 aws s3 cp _site s3://billsullivan.name/ --recursive --profile billsullivan
+
+aws cloudfront create-invalidation --distribution-id E2MVJDTKTGP8PO --paths "/*" --profile billsullivan
